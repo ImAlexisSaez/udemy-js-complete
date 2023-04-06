d1b0f36fa1e295dd792af8246be1197c2bf5bc30
Alexis <cucoalexis@hotmail.com>
2023-04-06 09:25:58 +0200

    Section 2

---
+++ s02/example03/script.js
@@ -0,0 +1,47 @@
+let age = 23;
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let fullAge = true;
+console.log(fullAge);
+
+fullAge = false;
+console.log(fullAge);
+
+let children;
+console.log(children);
+
+let mutantVariable = 23;
+console.log(mutantVariable);
+
+mutantVariable = "Jonas";
+console.log(mutantVariable);
+
+// Ejemplo de comentario en línea
+/*
+    Ejemplo
+    de
+    comentario
+    que
+    abarca
+    múltiples
+    líneas
+*/
+
+// ReferenceError
+// console.log(variableNoDeclarada);
+
+console.log(typeof 23);
+console.log(typeof firstName);
+console.log(typeof true);
+console.log(typeof children);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+console.log(year);
+console.log(typeof year);
