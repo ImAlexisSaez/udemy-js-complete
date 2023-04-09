be5811b134548457f94a7529032ca8ad66df7793
Alexis <cucoalexis@hotmail.com>
2023-04-09 15:22:42 +0200

    Section 2

---
+++ s02/example11/script.js
@@ -0,0 +1,39 @@
+// Ejemplo 1
+// const age = 18;
+
+// if (age === 18) {
+//     console.log("You just became an adult!");
+// }
+
+// console.log("18" == 18, "18" === 18);
+
+// Ejemplo 2
+// const favourite = prompt("What's your favorite number? ");
+// console.log(favourite, typeof favourite);
+
+// if (favourite == 23) {
+//     console.log("Cool! 23 is an amazing number!");
+// }
+
+// Ejemplo 3
+// const favourite = Number(prompt("What's your favorite number? "));
+
+// if (favourite === 23) {
+//     console.log("Cool! 23 is an amazing number!");
+// }
+
+// Ejemplo 4
+// const favourite = Number(prompt("What's your favorite number? "));
+
+// if (favourite === 23) {
+//     console.log("Cool! 23 is an amazing number!");
+// } else if (favourite === 7) {
+//     console.log("7 is also a cool number!");
+// } else {
+//     console.log("Number is not 7 or 23.");
+// }
+
+// Ejemplo 5
+const favourite = Number(prompt("What's your favorite number? "));
+
+if (favourite !== 23) console.log("Why not 23?");
