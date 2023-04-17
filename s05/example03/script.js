e6d9cbfed5c7d1c28ee24886fcf25c124f5b6d80
Alexis <cucoalexis@hotmail.com>
2023-04-17 12:24:31 +0200

    Section 5

---
+++ s05/example03/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const measureKelvin = function () {
+    const measurement = {
+        type: "temp",
+        unit: "celsius",
+        value: Number(prompt("Degrees celsius: ")),
+    };
+
+    // console.log(measurement);
+    // console.table(measurement);
+
+    // console.log(measurement.value);
+    // console.warn(measurement.value);
+    // console.error(measurement.value);
+
+    const kelvin = measurement.value + 273;
+    return kelvin;
+};
+
+console.log(measureKelvin());
